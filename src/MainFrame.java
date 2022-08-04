15f2900b30ecdbae6f29ae563b7281d2cd24aef3
OmnipotentBlade <91920612+OmnipotentBlade@users.noreply.github.com>
2022-08-04 15:41:49 -0400

    Add call to SideBarPanel

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,11 +1,11 @@
 package src;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Calendar;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -18,15 +18,21 @@ import javax.swing.UIManager;
 
 public class MainFrame extends JFrame implements ActionListener {
 
+    SideBarPanel sideBarPanel;
+
     MainFrame(Boolean startup) { // startup = whether mainframe was called from startupframe or not
         if (startup == true) {
             // add code if it requires condition above
         }
 
+        sideBarPanel = new SideBarPanel();
+        sideBarPanel.setPreferredSize(new Dimension(219, 650));
+        
+        this.add(sideBarPanel, BorderLayout.WEST);
+
         this.setTitle("Lock256");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allows the program to be closed just by clicking the "X" button at the top of the window
         this.setSize(1050, 650); // set size of frame
-        //this.setLayout(new BorderLayout());
         this.setResizable(false); // disables resizing of window (ex: fullscreening)
         this.setLocationRelativeTo(null);
         this.setVisible(true);

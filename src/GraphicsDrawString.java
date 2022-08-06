34da85ae3253ac0d7ae593606b5b1f8686a2408f
OmnipotentBlade <91920612+OmnipotentBlade@users.noreply.github.com>
2022-08-06 14:41:44 -0400

    Added new file

---
+++ src/GraphicsDrawString.java
@@ -0,0 +1,23 @@
+package src;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.JComponent;
+
+public class GraphicsDrawString extends JComponent {
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setPaint(new Color(227,193,255));
+        g2d.setFont(new Font("", 0, 18));
+        g2d.drawString("Omnipotent", 0, 31);
+    }
+}

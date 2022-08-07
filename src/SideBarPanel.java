ddcf63d910abe2275d3754a7e8186724a111ce77
OmnipotentBlade <91920612+OmnipotentBlade@users.noreply.github.com>
2022-08-07 16:52:56 -0400

    Added other icons

--- src/SideBarPanel.java
+++ src/SideBarPanel.java
@@ -16,9 +16,9 @@ import java.awt.Insets;
 
 public class SideBarPanel extends JPanel {
     
-    JLabel usernameLabel, userIconLabel;
+    JLabel usernameLabel, userLabel, favoritesLabel, loginsLabel, recentsLabel, passwordsLabel, reportLabel, settingsLabel;
     ImageIcon userImageIcon, favoritesImageIcon, recentsImageIcon, passwordsImageIcon, generateImageIcon, loginsImageIcon, reportImageIcon, settingsImageIcon;
-    GridBagConstraints gbc;
+    GridBagConstraints userGbc, usernameGbc, favoritesGbc, loginsGbc, recentsGbc, passwordsGbc, reportGbc, settingsGbc;
 
     SideBarPanel() {
         this.setLayout(new GridBagLayout());
@@ -31,30 +31,101 @@ public class SideBarPanel extends JPanel {
         reportImageIcon = new ImageIcon(FileMgmt.ICON_FLAG);
         settingsImageIcon = new ImageIcon(FileMgmt.ICON_GEAR);
 
-        gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridheight = 2;
-        gbc.gridwidth = 1;
-        gbc.weightx = 0;
-        gbc.weighty = 1;
-        gbc.ipadx = 20;
-        gbc.ipady = 15;
-        gbc.anchor = GridBagConstraints.FIRST_LINE_START;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-
-        userIconLabel = new JLabel(userImageIcon);
+        userLabel = new JLabel(userImageIcon);
         usernameLabel = new JLabel("USERNAME");
+        favoritesLabel = new JLabel(favoritesImageIcon);
+        loginsLabel = new JLabel(loginsImageIcon);
+        recentsLabel = new JLabel(recentsImageIcon);
+        passwordsLabel = new JLabel(passwordsImageIcon);
+        reportLabel = new JLabel(reportImageIcon);
+        settingsLabel = new JLabel(settingsImageIcon);
 
-        this.add(userIconLabel, gbc);
+        userGbc = new GridBagConstraints();
+        usernameGbc = new GridBagConstraints();
+        favoritesGbc = new GridBagConstraints();
+        loginsGbc = new GridBagConstraints();
+        recentsGbc = new GridBagConstraints();
+        passwordsGbc = new GridBagConstraints();
+        reportGbc = new GridBagConstraints();
+        settingsGbc = new GridBagConstraints();
 
-        gbc.gridx = 1;
-        gbc.ipadx = 0;
-        gbc.ipady = 31;
-        gbc.weightx = 1;
-        gbc.anchor = GridBagConstraints.PAGE_START;
+        userGbc.gridx = 0;
+        userGbc.gridy = 0;
+        userGbc.gridheight = 2;
+        userGbc.gridwidth = 1;
+        userGbc.weightx = 0;
+        userGbc.weighty = 1;
+        userGbc.ipadx = 20;
+        userGbc.ipady = 15;
+        userGbc.anchor = GridBagConstraints.FIRST_LINE_START;
+        userGbc.fill = GridBagConstraints.HORIZONTAL;
+
+        this.add(userLabel, userGbc);
+
+        usernameGbc.gridx = 1;
+        usernameGbc.gridy = 0;
+        usernameGbc.gridheight = 1;
+        usernameGbc.gridwidth = 1;
+        usernameGbc.weightx = 1;
+        usernameGbc.weighty = 1;
+        usernameGbc.ipadx = 0;
+        usernameGbc.ipady = 31;
+        usernameGbc.anchor = GridBagConstraints.PAGE_START;
+        usernameGbc.fill = GridBagConstraints.HORIZONTAL;
         
-        this.add(new GraphicsDrawString(), gbc);
+        this.add(new GraphicsDrawString(), usernameGbc);
+
+        favoritesGbc.gridx = 0;
+        favoritesGbc.gridy = 2;
+        favoritesGbc.gridheight = 1;
+        favoritesGbc.gridwidth = 1;
+        favoritesGbc.weightx = 0;
+        favoritesGbc.weighty = 1;
+        favoritesGbc.ipadx = 0;
+        favoritesGbc.ipady = 0;
+        favoritesGbc.anchor = GridBagConstraints.FIRST_LINE_START;
+        favoritesGbc.fill = GridBagConstraints.HORIZONTAL;
+
+        this.add(favoritesLabel, favoritesGbc);
+
+        recentsGbc.gridx = 0;
+        recentsGbc.gridy = 3;
+        recentsGbc.gridheight = 4;
+        recentsGbc.gridwidth = 1;
+        recentsGbc.weightx = 0;
+        recentsGbc.weighty = 1;
+        recentsGbc.ipadx = 0;
+        recentsGbc.ipady = 0;
+        recentsGbc.anchor = GridBagConstraints.LINE_START;
+        recentsGbc.fill = GridBagConstraints.HORIZONTAL;
+
+        this.add(recentsLabel, recentsGbc);
+
+        loginsGbc.gridx = 0;
+        loginsGbc.gridy = 4;
+        loginsGbc.gridheight = 4;
+        loginsGbc.gridwidth = 1;
+        loginsGbc.weightx = 0;
+        loginsGbc.weighty = 1;
+        loginsGbc.ipadx = 0;
+        loginsGbc.ipady = 0;
+        loginsGbc.anchor = GridBagConstraints.LAST_LINE_START;
+        loginsGbc.fill = GridBagConstraints.HORIZONTAL;
+
+        this.add(loginsLabel, loginsGbc);
+
+        passwordsGbc.gridx = 0;
+        passwordsGbc.gridy = 5;
+        passwordsGbc.gridheight = 5;
+        passwordsGbc.gridwidth = 1;
+        passwordsGbc.weightx = 0;
+        passwordsGbc.weighty = 0;
+        passwordsGbc.ipadx = 0;
+        passwordsGbc.ipady = 0;
+        passwordsGbc.anchor = GridBagConstraints.LAST_LINE_START;
+        passwordsGbc.fill = GridBagConstraints.HORIZONTAL;
+
+        this.add(passwordsLabel, passwordsGbc);
     }
 
     @Override

27ad83a307420762f1ec5d2ce2f0db4a3bd17df6
OmnipotentBlade <91920612+OmnipotentBlade@users.noreply.github.com>
2022-10-04 22:06:51 -0400

    Changed json write method

--- src/JSONMgmt.java
+++ src/JSONMgmt.java
@@ -1,35 +1,41 @@
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.HashMap;
 
 import org.json.simple.JSONObject;
 
 public class JSONMgmt {
 
-    public static void JSONWrite(String key, String value) throws FileNotFoundException {
-        // HashMap<String, Object> newEntry = new HashMap<String, Object>();
-        // newEntry.put(key, value);
+    static FileWriter file;
 
-        // PrintWriter pw = new PrintWriter(new FileOutputStream(new File("config.json"), true));
-        // pw.append(JSONObject.toJSONString(newEntry));
-        // pw.flush();
-        // pw.close();
+    public static void JSONWrite(String key, String value) throws FileNotFoundException {
 
         JSONObject jsonStream = new JSONObject();
         jsonStream.put(key, value);
         
         try {
-            FileWriter file = new FileWriter("config.json");
+            file = new FileWriter("config.json");
+            file.write(jsonStream.toJSONString());
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void JSONWrite(String key, char[] value) throws FileNotFoundException {
+
+        JSONObject jsonStream = new JSONObject();
+        jsonStream.put(key, value);
+        
+        try {
+            file = new FileWriter("config.json");
             file.append(jsonStream.toJSONString());
             file.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     public static void JSONRead(String key, String value) throws FileNotFoundException {
         
     }
